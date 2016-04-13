af23e67b02eb483f102f9fe6a9e44e42ef84d717
ziyang <ziyang0621@gmail.com>
2016-04-12 20:42:15 -0700

    updated project

--- ZTDropDownTextField/ZTDropDownTextField-Bridging-Header.h
+++ ZTDropDownTextField/ZTDropDownTextField-Bridging-Header.h
@@ -1,9 +1,6 @@
 //
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
-
-//#import <pop/POP.h>
-
 #if __has_include(<ZTDropDownTextField/ZTDropDownTextField-BridgingHeader.h>)
 @import pop;
 #else
